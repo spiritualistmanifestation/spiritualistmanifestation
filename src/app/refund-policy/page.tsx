0f0b7d61a8156389ee02dbb358f2e2a895c8793f
Patrick Marcus <patrickmarcus580@gmail.com>
2025-09-08 22:28:52 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/refund-policy/page.tsx
+++ src/app/refund-policy/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 
-export default function RefundPolicyPage()_ {
+export default function RefundPolicyPage() {
     const [lastUpdated, setLastUpdated] = useState('');
 
     useEffect(() => {
