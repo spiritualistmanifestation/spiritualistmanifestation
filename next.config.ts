3e0ffe56bee3df7cbe8464f21f9f57c77bc85598
Patrick Marcus <patrickmarcus580@gmail.com>
2025-09-08 22:17:09 +0000

    in the services page on love spell section pls update the image that sho

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'witchsymbols.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
